60decffd32f9bf065bb82777f43dc0cbc0862536
Ohene Gyan <Janmillz@yahoo.com>
2017-09-29 20:19:00 +0100

    modified

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,26 @@ function Contact(first, last) {
   this.lastName = last;
 }
 
+// //business logic
+// function Contact(first, last) {
+//   this.firstName = first;
+//   this.lastName = last;
+// }
 //business logic
 function Contact(first, last) {
   this.firstName = first;
   this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, county) {
+  this.street = street;
+  this.city = city;
+  this.county = county;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
 }
 
 // user interface logic
